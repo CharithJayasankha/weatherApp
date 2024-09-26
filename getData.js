8cccd0cc86a6ebde8043a7b06d94ab5b54442ad5
Your Name <you@example.com>
2024-09-26 15:42:38 +0530

    adding hourly forecast

--- getData.js
+++ getData.js
@@ -9,12 +9,12 @@ let pressure_mb = document.getElementById("pressure_mb");
 let uv = document.getElementById("uv");
 let humidity = document.getElementById("humidity");
 let localTime = document.getElementById("localTime");
+let hourlyForecast = document.getElementById("hourlyForecast");
 
 let apiKey = "a6fe6ee90f0944f8845143548242908";
-
+let array = [];
 function fetchWeather(city) {
-  let url = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`;
-  //'https://api.weatherapi.com/v1/current.json?key=a6fe6ee90f0944f8845143548242908&q=Colombo'
+  let url = `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${city}&days=5`;
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
@@ -26,9 +26,9 @@ function fetchWeather(city) {
         descText.textContent = data.current.condition.text;
 
         let dateTime = data.location.localtime;
-        let date = dateTime.split(" ")[1];
-        console.log(date);
-        localTime.textContent = date;
+        let time = dateTime.split(" ")[1];
+
+        localTime.textContent = time;
         temp_c.textContent = `${data.current.temp_c}°C`;
         feelsLike_c.textContent = `Feels Like: ${data.current.feelslike_c}°C`;
 
@@ -36,6 +36,31 @@ function fetchWeather(city) {
         pressure_mb.textContent = `${data.current.pressure_mb} mb`;
         uv.textContent = `${data.current.uv}`;
         humidity.textContent = `${data.current.humidity}%`;
+
+        let forecastDay = data.forecast.forecastday;
+
+        let hours = forecastDay[0].hour;
+
+        let hourIndex = Number(time.split(":")[0]) + 1;
+
+        let count = 0;
+        for (; hourIndex < hours.length; hourIndex++) {
+          if (count == 8) break;
+
+          let card = `<div
+                class="card text-center shadow col-6 col-sm-4 col-lg-2 p-2 m-1"
+                style="background-color: #31b5e6; width:150px"
+              >
+                <div class="card-body p-1">
+                  <p>${hourIndex}:00</p>
+                  <img src="${"http:" + hours[hourIndex].condition.icon}">
+                  <h2>${hours[hourIndex].temp_c}°C</h2>
+                </div>
+              </div>`;
+
+          hourlyForecast.innerHTML += card;
+          count++;
+        }
       } else {
         console.error("Invalid data received from API");
       }
@@ -48,6 +73,7 @@ searchBox.addEventListener("keypress", function (e) {
     let city = searchBox.value.trim();
     if (city) {
       fetchWeather(city);
+      hourlyForecast.innerHTML = "";
     }
   }
 });
